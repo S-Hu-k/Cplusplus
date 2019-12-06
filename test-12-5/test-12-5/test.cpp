9400a0d351c4baf66d17fe49cd3e4810c91599f7
胡一凯 <2096136603@qq.com>
2019-12-06 18:36:22 +0800

    类与对象

--- test-12-5/test-12-5/test.cpp
+++ test-12-5/test-12-5/test.cpp
@@ -1,14 +1,141 @@
+
 #include<iostream>
+#include<stdio.h>
+#include<time.h>
+#include"datetime.h"
 using namespace std;
 
+void main()
+{
+	CDateTime dt;
+	//cout<<dt<<endl;
+	dt.DShowDateTime();
+	//dt1 = dt + 100
+}
+ 
 
 
-
+/*
+class List;
+class ListNode
+{
+	class List;
+public:
+	ListNode():m_data(0),next(nullptr)
+	{}
+	ListNode(int data ,ListNode *n=nullptr)
+	{}
+	~ListNode()
+	{}
+private:
+	int m_data;
+	ListNode* next;
+};
+class List
+{
+public:
+	List():head(nullptr)
+	{}
+private:
+	ListNode* head;
+};
+void main()
+{
+	List mylist;
+}
+/*
 class Test
 {
 	friend ostream& operator<<(ostream& out, const Test& t);
 public:
-	explicit Test(int data = 0) :m_data(data), m_count(0)//只能用在构造函数中
+	explicit Test(int data = 0) :m_data(data)//只能用在构造函数中
+	{
+		m_count++;
+	}
+	Test& operator=(const Test& t)
+	{
+		if (this != &t)
+		{
+			m_data = t.m_data;
+		}
+		return *this;
+	}
+	~Test()
+	{
+		 m_count--;
+	}
+public:
+	void Increment_Count()
+	{
+		m_count++;
+	}
+public://让对象强转
+	operator int()
+	{
+		return m_data;
+	}
+public:
+	 int GetUseCount()const
+	{
+		return m_count;
+	}
+private:
+//public:
+	int m_data;
+	static int m_count;//静态 大家共享的 
+};
+///////////////////////////////////////////////
+int Test::m_count = 0;
+////////////////////////////////////////////
+ostream& operator<<(ostream& out, const Test& t)
+{
+	out << t.m_data;
+	return out;
+}
+void main()
+{
+
+}
+/*
+void main()
+{
+
+	
+	Test t1;
+	Test t2;
+
+	cout << t1.GetUseCount() << endl;
+	{
+		Test t3;
+		cout << t1.GetUseCount() << endl;
+	}
+		
+
+	
+	cout << t1.GetUseCount() << endl;
+
+	//cout << t2.GetUseCount() << endl;
+
+	//t1.Increment_Count();
+	//t2.Increment_Count();
+}
+/*
+void main()
+{
+	Test t1;
+	Test t2;
+	t1.Increment_Count();
+	t2.Increment_Count();
+}
+
+//explicit  显式  不允许发生隐式转换 //只能用在构造函数中
+
+/*
+class Test
+{
+	friend ostream& operator<<(ostream& out, const Test& t);
+public:
+	explicit Test(int data = 0) :m_data(data)//只能用在构造函数中
 	{
 		
 	}
