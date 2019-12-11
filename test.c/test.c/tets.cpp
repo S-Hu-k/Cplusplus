213c0b8311bda689b3fc6dd11fdb89bace9cede4
胡一凯 <2096136603@qq.com>
2019-12-11 19:46:15 +0800

    内存管理

---
+++ test.c/test.c/tets.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+    int* p1 = new int[10];//中括号用数组的方式申请
+    cout << *p1 << endl;
+    delete [10]p1;//释放也要用中括号
+}
+/*
+void main()
+{
+    int* p1 = new int(10);//小括号是赋初值
+    cout << *p1 << endl;
+    delete p1;
+ }
+ */
