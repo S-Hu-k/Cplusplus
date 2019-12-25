09ff20f22d39ddf36daded3b5986af82538528e9
胡一凯 <2096136603@qq.com>
2019-12-25 20:24:10 +0800

    提交代码

---
+++ test.c-12-24/test.c-12-24/test.c
@@ -0,0 +1,48 @@
+
+#include<stdio.h>
+int main() 
+{
+    int i, j, s, k, tag;
+    int x = 0;
+    printf("请输入要操作的数:>");
+    scanf_s("%d", &x);
+    for (i = 1; i <= x; i++)
+    {
+        s = 0;
+        for (j = 1; j <= i / 2; j++) 
+        {
+            if (i % j == 0)
+                s = s + j;
+        }
+        if (s == i)
+        {
+            printf("%d\n", i);
+           
+        }
+    }
+    return 0;
+}
+
+
+/*
+#include <stdio.h>
+int main()
+{
+	int N = 0;
+	scanf_s("%d",&N);
+	for(int i=1;i<=100;i++)
+	{
+		
+		for (int j = i; j <= 100; j++)
+
+		{
+				if ((i + j) * (j - i + 1) / 2 == N)
+				{
+					printf("i=%d \n", i);
+					printf("j=%d \n", j);
+				}
+		}
+	}		
+	return 0;
+}
+*/
