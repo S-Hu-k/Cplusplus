9aede47db85eb43e1c1187bf6ccf52583f2b094b
胡一凯 <2096136603@qq.com>
2019-12-26 21:26:20 +0800

    提交代码

--- test.c-12-24/test.c-12-24/test.c
+++ test.c-12-24/test.c-12-24/test.c
@@ -1,4 +1,4 @@
-
+/*
 #include<stdio.h>
 int main() 
 {
@@ -46,3 +46,20 @@ int main()
 	return 0;
 }
 */
+#include<stdio.h>
+int main()
+{
+    int flag = 1;
+    int x = 1;
+    float a = 1.0;
+    int b = 0;
+    while (fabs(a) >= 0.0001)
+    {
+        a = flag * 1.0 / x;
+        b = b + a;
+        flag = -flag;
+        x = x + 2;
+    }
+    printf("%p", &a);
+
+}
