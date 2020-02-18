7cba47d509c3d64399fda799a10450d00656b670
胡一凯 <2096136603@qq.com>
2020-02-18 17:09:58 +0800

    string模拟实现

---
+++ test-2-18/test-2-18/test.cpp
@@ -0,0 +1,71 @@
+#include<string>
+#include<iostream>
+istream& operator>>(istream& is, String& s1)
+{
+	char* str1 = new char[10];
+	int count = 1;
+	int cpity = 10;
+	char* temp = str1;
+	while (((*temp = getchar()) == ' ') || (*temp == '\n'));
+	while (1)
+	{
+
+		if (count >= cpity)
+		{
+			str1 = (char*)realloc(str1, cpity *= 2);
+			temp = str1 + count - 1;
+		}
+		else if ((*temp == ' ') || *temp == '\n')
+		{
+			*temp = '\0';
+			break;
+		}
+		*++temp = getchar();
+		count++;
+	}
+	s1.str = str1;
+	s1.capacity = cpity;
+	s1.len = count;
+	return is;
+
+
+
+
+
+/*
+istream& operator>>(istream& _cin, bit::string& s)
+{
+	static const size_t default_buf_size = 10;
+	int capacity = default_buf_size;
+	char* str = (char*)malloc(sizeof(char) * default_buf_size);
+	char* buf = str;
+
+	size_t count = 0;
+
+	//跳过字符串起始位置的空格或者换行
+	while ((*buf = getchar()) == ' ' || (*buf == '\n'));
+
+	for (;;)
+	{
+		if (*buf == '\n' || *buf == ' ')
+		{
+			*buf = '\0';
+			break;
+		}
+		else if (count >= capacity)
+		{
+			capacity *= 2;
+			str = (char*)realloc(str, capacity);
+			//重新定位buf的位置
+			buf = str + count;
+		}
+		*++buf = getchar();
+		count++;
+	}
+
+	s.m_str = str;
+	s.m_capacity = capacity;
+	s.m_size = count;
+	return _cin;
+}
+*/
