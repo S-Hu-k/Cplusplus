9e2c7d16b7e9aa687df53815dc7577eff53a6acd
胡一凯 <2096136603@qq.com>
2020-03-12 17:38:08 +0800

    密码评分

---
+++ Project1/Project1/test.h
@@ -0,0 +1,10 @@
+/*
+public static Safelevel GetPwdSecurityLevel(string pPasswordStr)
+{
+void JudgeScoreByLenth(string pPasswordStr);
+void JudgeScoreByLetter(string pPasswordStr);
+void JudgeScoreByNumber(string pPasswordStr);
+void JudgeScoreBySingle(string pPasswordStr);
+void JudgeScoreByEncourage(string pPasswordStr);
+}
+*/
