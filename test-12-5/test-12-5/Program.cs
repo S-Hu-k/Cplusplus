beea4169546ec6c51d61ded67e96e0fbf9094d6b
胡一凯 <2096136603@qq.com>
2020-03-19 11:16:03 +0800

    每日练习

---
+++ test-12-5/test-12-5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test_12_5
+{
+    class Program
+    {
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
