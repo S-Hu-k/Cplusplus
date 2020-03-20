651252c18d9327468186ec9128060f57b1e2b8aa
胡一凯 <2096136603@qq.com>
2020-03-20 16:28:38 +0800

    每日练习

---
+++ test-3-19/test-3-19/test.c
@@ -0,0 +1,27 @@
+/*
+#include<stdio.h>
+print(char* s)
+{
+	if (*s)
+	{
+		print(++s);
+		printf("%c", *s);
+	}
+}
+int main()
+{
+	char str[] = "geneius";
+	print(str);
+}
+/*
+struct x
+{
+	short s;
+	int i;
+	char c;
+};
+int main()
+{
+	printf("%d", sizeof(struct x));
+	return 0;
+}*/
