a957df8adffca46311a6369ea634966cecf0d83f
胡一凯 <2096136603@qq.com>
2020-03-30 12:29:21 +0800

    继承 多态

--- test-3-14-继承 多态/test-3-14-继承 多态/test.cpp
+++ test-3-14-继承 多态/test-3-14-继承 多态/test.cpp
@@ -1,3 +1,131 @@
+
+#include<iostream>
+using namespace std;
+class Base
+{
+public:
+	int m_n;
+};
+
+//虚拟继承
+class A : virtual public Base   //A 有自己的虚基表
+{
+public:
+	int m_a;
+};
+class B : virtual public Base  //B 有自己的虚基表
+{
+public:
+	int m_b;
+};
+class D : public A, public B
+{
+public:
+	int m_d;
+};
+
+void main()
+{
+	D d;
+	d.m_d = 0;
+	d.m_a = 1;
+	d.m_b = 2;
+
+	d.A::m_n = 3;
+
+}
+/*
+#define DISPLAY
+class Base
+{
+public:
+	Base(int a) :m_a(a), m_b(a)
+	{
+#ifdef DISPLAY
+		cout << "Base::Base()" << endl;
+#endif
+	}
+	~Base()
+	{
+#ifdef DISPLAY
+		cout << "Base::~Base()" << endl;
+#endif
+	}
+public:
+	void fun()
+	{
+		cout << "Base::fun()" << endl;
+	}
+	void fun(int, int)
+	{
+		cout << "Base::fun(int,int)" << endl;
+	}
+	void show()
+	{
+		cout << "Base::show()" << endl;
+	}
+public:
+	int m_a = 0;
+	int m_b = 0;
+};
+class Base1
+{
+public:
+	Base1(int a, int b) :m_a1(a), m_b1(b)
+	{
+#ifdef DISPLAY
+		cout << "Base1::Base1()" << endl;
+#endif
+	}
+	~Base1()
+	{
+#ifdef DISPLAY
+		cout << "Base1::~Base1()" << endl;
+#endif
+	}
+public:
+	void fun()
+	{
+		cout << "Base1::fun()" << endl;
+	}
+	void fun(int, int)
+	{
+		cout << "Base1::fun(int,int)" << endl;
+	}
+	void show()
+	{
+		cout << "Base1::show()" << endl;
+	}
+public:
+	int m_a1 = 0;
+	int m_b1 = 0;
+};
+//多继承
+class D : public Base1, virtual public Base
+{
+public:
+	D() : bs(0), Base(1), bs1(1, 2), Base1(1, 2)
+	{
+		cout << "D::D()" << endl;
+	}
+	~D()
+	{
+		cout << "D::~D()" << endl;
+	}
+private:
+	int m_x;
+	int m_y;
+	Base bs;
+	Base1 bs1;
+};
+void main()
+{
+	//1 先按照继承的顺序构造父类
+	//2 按照对象的顺序实例化对象成员
+	//3 在构造子类
+	D d;
+}
+/*
 #include<iostream>
 using namespace std;
 class Base
