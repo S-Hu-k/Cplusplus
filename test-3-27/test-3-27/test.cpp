c91a28edf732b27c22c0f01ababff5de2e7016e2
胡一凯 <2096136603@qq.com>
2020-03-31 18:09:04 +0800

    每日练习

---
+++ test-3-27/test-3-27/test.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+/*
+int main()
+{
+    long CatLegth, r;
+    cin >> CatLegth;
+    cin>> r;
+    long C = 2 * r * 3.14;
+    if (CatLegth > C)
+        cout << "No" << endl;
+    else
+        cout << "Yes" << endl;
+}
+
+
+#include <iostream>
+using namespace std;
+
+/*一个double就可以解决，不用考虑用字符串做大整数乘除法*/
+
+int main()
+{
+    double n, r;
+    while (cin >> n >> r)
+    {
+        if (n > 6.28 * r)
+            cout << "No" << endl;
+        else
+            cout << "Yes" << endl;
+    }
+    return 0;
+}*/
