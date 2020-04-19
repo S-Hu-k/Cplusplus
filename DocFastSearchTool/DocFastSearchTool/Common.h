592858d4614a43de2f9751a1b48f119ca3306fb0
胡一凯 <2096136603@qq.com>
2020-04-19 16:20:11 +0800

    项目副本

---
+++ DocFastSearchTool/DocFastSearchTool/Common.h
@@ -0,0 +1,31 @@
+/*#pragma once
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<io.h>
+
+
+
+using namespace std;*/
+#pragma once
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+
+#include"sqlite\sqlite3.h"
+
+#include<io.h>
+
+#include<thread>
+#include<chrono>
+
+#include<windows.h>
+
+using namespace std;
+
+#define SQL_BUFFER_SIZE 256
+#define MAX_TITLE_BUFFER 128
