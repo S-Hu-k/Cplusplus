ff4f988a7ffc70989d009816866350ac7d3dbc40
胡一凯 <2096136603@qq.com>
2020-05-05 23:06:02 +0800

    BST

---
+++ 四月/test-5-5/test-5-5/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+template<class Type>
+class BSTNode
+{
+private:
+	Type data;
+	BSTNode<Type>* leftChild;
+	BSTNode<Type>* rightChild;
+};
+int main()
+{
+
+}
