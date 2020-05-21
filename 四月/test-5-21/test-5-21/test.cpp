fdc63fef42bcbd08ef5004278be4aa91835581b3
胡一凯 <2096136603@qq.com>
2020-05-21 21:12:31 +0800

    RBTree

---
+++ 四月/test-5-21/test-5-21/test.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+typedef enum { RED = 0, BLACK = 1 }Colour_type;
+
+template<class	Type>
+class RBTree;
+
+template<class	Type>
+class RBTreeNode
+{
+	friend class RBTree<Type>;
+
+public:
+	RBTreeNode(const Type&d=Type())
+		:data(d),leftChild(nullptr),rightChild(nullptr),parent(nullptr),colour(RED)
+	{}
+	~RBTreeNode()
+	{}
+private:
+	Type data;
+	RBTreeNode<Type>* leftChild;
+	RBTreeNode<Type>* rightChild;
+	RBTreeNode<Type>*  parent;
+	Colour_type        colour;
+
+};
+
+template<class	Type>
+class RBTree
+{
+public:
+	RBTree():root(NIL),NIL(_BuyNode)
+	{
+		NIL->leftChild = NIL->rightChild = NIL;
+		NIL->colour = BLACK;
+	}
+protected:
+	RBTreeNode<Type>* _BuyNode(const Type&x=Type())
+	{
+
+		RBTreeNode<Type>* s = new RBTreeNode<Type>(x);
+		s->leftChild = s->rightChild = NIL;	
+		return s;
+	}
+
+private:
+	RBTreeNode<Type>* NIL;
+	RBTreeNode<Type>* root;
+
+
+};
+int  main()
+{
+	RBTree<int> rb;
+	return 0;
+}
