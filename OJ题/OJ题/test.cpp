7cedba72244ba97c0ae41424ca85a72202538754
胡一凯 <2096136603@qq.com>
2020-11-23 10:51:48 +0800

    OJ题

--- OJ题/OJ题/test.cpp
+++ OJ题/OJ题/test.cpp
@@ -1,4 +1,124 @@
 
+//输入两个字符串，从第一字符串中删除第二个字符串中所有的字符。
+//例如，输入”They are students.”和”aeiou”，则删除之后的第一个字符串变成”Thy r stdnts.”
+//
+//输入描述 :
+//每个测试输入包含2个字符串
+//输出描述 :
+//输出删除后的字符串
+//示例1
+//输入
+//They are students.aeiou
+//输出
+//Thy r stdnts.
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//    string str1, str2;
+//    //使用 cin，会导致遇到空格就结束的情况
+//    getline(cin, str1);
+//    getline(cin, str2);
+//    //创建哈希数组计算字符出现的次数
+//    int hashtable[256] = { 0 };
+//    for (size_t i = 0; i < str2.size(); i++) {
+//        hashtable[str2[i]]++;
+//    }
+//    //创建临时对象，然后将没有重复的字符，返回到 ret 对象中
+//    string ret;
+//    for (size_t i = 0; i < str1.size(); i++) {
+//        if (hashtable[str1[i]] == 0)
+//            ret += str1[i];
+//    }
+//
+//    cout << ret << endl;
+//    return 0;
+//}
+
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//    string s1, s2;
+//    getline(cin, s1);
+//    getline(cin, s2);
+//    for (int i = 0; i < s2.size(); i++)
+//    {
+//        int index;
+//        while ((index = s1.find(s2[i])) != -1)
+//        {
+//            s1.erase(index, 1);
+//        }
+//    }
+//    cout << s1 << endl;
+//    return 0;
+//}
+
+
+
+
+
+//牛牛举办了一次编程比赛, 参加比赛的有3* n个选手,【https://www.nowcoder.com/questionTerminal/6736cc3ffd1444a4a0057dee89be789b】
+//每个选手都有一个水平值a_i.现在要将这些选手进行组队,
+//一共组成n个队伍, 即每个队伍3人.牛牛发现队伍的水平值等于该队伍队员中第二高水平值。
+//例如 :
+//一个队伍三个队员的水平值分别是3, 3, 3.那么队伍的水平值是3
+//一个队伍三个队员的水平值分别是3, 2, 3.那么队伍的水平值是3
+//一个队伍三个队员的水平值分别是1, 5, 2.那么队伍的水平值是2
+//为了让比赛更有看点, 牛牛想安排队伍使所有队伍的水平值总和最大。
+//如样例所示 :
+//如果牛牛把6个队员划分到两个队伍
+//如果方案为 :
+//team1: {1, 2, 5}, team2 : {5, 5, 8}, 这时候水平值总和为7.
+//而如果方案为 :
+//    team1 : {2, 5, 8}, team2 : {1, 5, 5}, 这时候水平值总和为10.
+//    没有比总和为10更大的方案, 所以输出10.
+//
+//    输入描述 :
+//    输入的第一行为一个正整数n(1 ≤ n ≤ 10 ^ 5)
+//
+//    第二行包括3* n个整数a_i(1 ≤ a_i ≤ 10 ^ 9), 表示每个参赛选手的水平值.
+//
+//    输出描述 :
+//    输出一个整数表示所有队伍的水平值总和最大值.
+//
+//    输入例子 :
+//    2
+//    5 2 8 5 1 5
+//
+//    输出例子 :
+//    10
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//    int n;
+//    while (cin >> n) {
+//        long long sum = 0;//因为其累加和的数据较大
+//        vector<int> a;
+//        a.resize(3 * n);
+//        for (int i = 0; i < 3 * n; i++) {
+//            cin >> a[i];
+//        }
+//    std:sort(a.begin(), a.end());//排序
+//        for (int i = n; i <= 3 * n - 2; i += 2) {//取每一个中间值
+//            sum += a[i];//进行累加
+//        }
+//        cout << sum << endl;
+//    }
+//    return 0;
+//}
+
+
+
+
+/*
 #include<iostream>
 using namespace std;
 struct a
