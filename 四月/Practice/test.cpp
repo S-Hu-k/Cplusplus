3615e8dbc4dc7178f72c68569fe65129c0b179ac
胡一凯 <2096136603@qq.com>
2020-12-03 17:25:52 +0800

    oj

--- 四月/Practice/test.cpp
+++ 四月/Practice/test.cpp
@@ -1,5 +1,449 @@
 
 #include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+    long score = 0;
+    string str;
+    getline(cin , str);
+
+    if (str.size() <= 4)
+        score += 5;
+    else if (str.size() > 5 && str.size() < 7)
+        score += 10;
+    else if (str.size() >= 8)
+        score += 25;
+    int c1=0, c2=0, c3=0, c4=0;
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            c1++;
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            c2++;
+        if (str[i] >= '0' && str[i] <= '9')
+            c3++;
+        if ((str[i] >= 0x21 && str[i] <= 0x2F) ||
+            (str[i] >= 0x3A && str[i] <= 0x40) ||
+            (str[i] >= 0x5B && str[i] <= 0x60) ||
+            (str[i] >= 0x7B && str[i] <= 0x7E))
+            c4++;
+    }
+    if (c1 == 0 && c2 == 0)
+        score += 0;
+    else if ((c1 != 0 && c2 == 0) || (c1 == 0 && c2 != 0))
+        score += 10;
+    else if (c1 != 0 && c2 != 0)
+        score += 25;
+    
+    if (c3 == 0)
+        score += 0;
+    else if (c3 == 1)
+        score += 10;
+    else if (c3 > 1)
+        score += 20;
+
+    if (c4 == 0)
+        score += 0;
+    else if (c4 == 1)
+        score += 10;
+    else if (c4 > 1)
+        score += 25;
+
+    if ((c1 != 0 && c2 != 0) && c3 != 0 && c4 != 0)
+        score += 5;
+    else if (((c1 != 0 && c2 == 0) || (c1 == 0 && c2 != 0)) && c3 != 0 && c4 != 0)
+        score += 3;
+    else if (c4 == 0 && c3 != 0 &&
+        ((c1 != 0 && c2 == 0) || (c1 == 0 && c2 != 0)))
+        score += 2;
+
+
+
+    if (score >= 90)
+        cout << "VERY_SECURE" << endl;
+    else if (score >= 80)
+        cout << "SECURE" << endl;
+    else if (score >= 70)
+        cout << "VERY_STRONG" << endl;
+    else if (score >= 60)
+        cout << "STRONG" << endl;
+    else if (score >= 50)
+        cout << "AVERAGE" << endl;
+    else if (score >= 25)
+        cout << "WEAK" << endl;
+    else
+        cout << "VERY_WEAK" << endl;
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+#include<iostream>
+#include<vector>
+using namespace std;
+class Board
+{
+public:
+    bool checkWon(vector<vector<int> > board)
+    {
+        int sum = 0;
+        int a = board.size();
+        for (int i = 0; i < a; ++i)
+        {
+            int sum = 0;
+            for (int j = 0; j < a; ++j)
+            {
+
+                sum += board[i][j];
+            }
+            if (sum == a)
+                return true;
+        }
+        /////////////////////////////////////////////////
+        for (int i = 0; i < a; ++i)
+        {
+            int sum = 0;
+            for (int j = 0; j < a; ++j)
+            {
+                sum += board[j][i];
+            }
+            if (sum == a)
+                return true;
+        }
+        ///////////////////////////////////////////////////////////
+        for (int i = 0; i < a; i++)
+        {
+            sum = 0;
+            sum += board[i][i];
+        }
+        if (sum == a) 
+        {
+            return true;
+        }
+        ///////////////////////////////////////////////
+        for (int i = 0; i < a; i++)
+        {
+            sum += board[i][a - i - 1];
+        }
+        if (sum == a)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+/*
+class Board
+{
+public:
+    bool checkWon(vector<vector<int> > board)
+    {
+        int a = board.size();
+        for (int i = 0; i < a; ++i)
+        {
+            int sum = 0;
+            for (int j = 0; j < a; ++j)
+            {
+
+                sum += board[i][j];
+            }
+            if (sum == a)
+                return true;
+        }
+        /////////////////////////////////////////////////
+        for (int i = 0; i < a; ++i)
+        {
+            int sum = 0;
+            for (int j = 0; j < a; ++j)
+            {
+                sum += board[j][i];
+            }
+            if (sum == a)
+                return true;
+        }
+        ///////////////////////////////////////////////////////////
+        for (int i = 0; i < a; ++i)
+        {
+            int sum = 0;
+            for (int j = 0; j < a; ++j)
+            {
+
+                sum += board[i][i];
+            }
+            if (sum == a)
+                return true;
+        }
+        ///////////////////////////////////////////////
+        for (int i = 0; i < a; ++i)
+        {
+            int sum = 0;
+            for (int j = 0; j < a; ++j)
+            {
+
+                sum += board[j][j];
+            }
+            if (sum == a)
+                return true;
+        }
+        return false;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+/*
+#include<iostream>
+using namespace std;
+struct a
+{
+    void foo() { printf("foo"); }
+    virtual void bar() { printf("bar"); }
+    a() { bar(); }
+};
+struct b :a
+{
+    void foo() {
+        printf("b_foo")
+            ;
+    }void bar() {
+        printf("b_bar");
+    }
+};
+int main()
+{
+    a* p = new b;
+    p->foo();
+    p->bar();
+}
+/*
+int func(int x)
+{
+    int count = 0;
+    while (x)
+    {
+        count++;
+        x = x & (x - 1);
+    }
+    return count;
+}
+int main()
+{
+    cout << func(9999) << endl;
+}
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    if (i < 0)
+    {
+        printf("****\n");
+    }
+    else
+        printf("%%%%\n");
+}
+
+/*
+#include<iostream>
+using namespace std;
+int PathNum(int n, int m)
+{
+    if (n > 1 && m > 1)
+        return PathNum(n - 1, m) + PathNum(n, m - 1);
+    else if (((n == 1) && (m >= 1)) || ((n >= 1) && (m == 1)))
+        return n + m;
+    else //格子数为0，直接返回0
+        return 0;
+}
+
+int main()
+{
+    int n, m;
+    while (cin >> n >> m)
+        cout << PathNum(n, m) << endl;
+    return 0;
+}
+/*
+#include<iostream>
+using namespace std;
+
+int Go(int m, int n) {
+    if (m < 0 || n < 0) {
+        return 0;
+    }
+    else if (n == 0 || m == 0) {
+        return 1;
+    }
+    else
+        return Go(n - 1, m) + Go(n, m - 1);
+}
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    Go(m, n);
+    cout << Go(m, n) << endl;
+    system("pause");
+    return 0;
+}
+
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+int go(int m, int n)
+{
+    if (m <= 0 || n <= 0)
+        return 0;
+    else if (m == 1 || n == 1)
+        return 1;
+    else
+        return go(m, n - 1) + go(m - 1, n);
+
+}
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    go(m, n);
+    cout << go(m, n) << endl;
+
+
+}
+
+
+
+
+/*
+#include<iostream>
+using namespace std;
+class test
+{
+public:
+    int a;
+    int b;
+    virtual void fun()
+    {}
+    test(int temp1 = 0, int temp2 = 0)
+    {
+        a = temp1;
+        b = temp2;
+    }int geta()
+    {
+        return a;
+    }
+    int getb()
+    {
+        return b;
+    }
+};
+int main()
+{
+    test obj(5, 10);
+    int* pint = (int*)&obj;
+    *(pint + 0) = 100;
+    *(pint + 1) = 200;
+    cout << obj.geta() << endl;
+    cout << obj.getb() << endl;
+}
+
+/*
+#include<iostream>
+using namespace std;
+class myclass
+{
+public:
+    myclass(int i = 0) { cout << 1; }
+    myclass(const myclass& x) { cout << 2; }
+    myclass& operator =(const myclass& x) { cout << 3; return *this; }
+    ~myclass() { cout << 4; }
+};
+int main()
+{
+    myclass obj1(1), obj2(2), obj3(obj1);
+    return 0;
+}
+
+
+
+/*
+#include<iostream>
+using namespace std;
+void func(char** m)
+{
+    ++m;
+    cout << *m << endl;
+}
+int main()
+{
+     char* a[] = { "morning","afternoon","evening" };
+    char** p;
+    p = a;
+    func(p);
+    return 0;
+}
+/*
+#include<iostream>
+using namespace std;
+int fun(int n)
+{
+    int a = 0;
+   a ^= (1 << 5) - 1;
+    return a;
+}
+void func(int& nvall)
+{
+
+}
+int main()
+{
+    int b = 0;
+    func(b);
+    cout << fun(21) << endl;
+}
+/*
+#include<iostream>
 #include<vector>
 #include<string>
 using namespace std;
@@ -45,15 +489,6 @@ int mian()
         cout << "none" << endl;
     return 0;
 }
-
-
-
-
-
-
-
-
-
 
 
 
