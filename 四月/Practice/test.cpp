2a61d61e0dcd782e285b1b29e86a178635b9c8b6
胡一凯 <2096136603@qq.com>
2020-12-20 23:08:32 +0800

    oj

--- 四月/Practice/test.cpp
+++ 四月/Practice/test.cpp
@@ -1,3 +1,41 @@
+
+class Solution {
+public:
+    /**
+     *
+     * @param A int整型一维数组
+     * @param n int A数组长度
+     * @param target int整型
+     * @return int整型
+     */
+    int search(int* A, int n, int target) {
+        // write code here
+        int left = 0;
+        int right = n - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (A[mid] == target)
+                return mid;
+            else if (A[left] <= A[mid]) {
+                if (target < A[mid] && target >= A[left])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+
+            }
+            else {
+                if (target > A[mid] && target <= A[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+**************************************************************************
 public class Solution {
     public ListNode FindKthToTail(ListNode head, int k) {
 
