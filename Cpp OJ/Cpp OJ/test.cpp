10871ab6c5cd49c83dbf60a86dfb19d7ce4f3155
胡一凯 <2096136603@qq.com>
2021-01-21 21:57:24 +0800

    oj

--- Cpp OJ/Cpp OJ/test.cpp
+++ Cpp OJ/Cpp OJ/test.cpp
@@ -2,6 +2,172 @@
 
 /********************************************************************************************************************/
 
+MP3光标位置
+
+#include <iostream> 
+#include <string> 
+using namespace std;
+int main() {
+    //歌曲数量    
+    int n;
+    //命令    
+    string order;
+    while (cin >> n >> order) {
+        //将n首歌曲编号1：n , num为光标当前所在歌曲的编号,first为当前屏幕显示
+        //页的第一首歌曲的编号        
+        int num = 1, first = 1;
+        //歌曲总数不超过4时，所有歌曲一页即可显示完，不需翻页，first始终不变		
+        if (n <= 4) {
+            for (int i = 0; i < order.size(); i++) {
+                if (num == 1 && order[i] == 'U') {
+                    num = n;
+                }
+                else if (num == n && order[i] == 'D') {
+                    num = 1;
+                }
+                else if (order[i] == 'U') {
+                    num--;
+                }
+                else {
+                    num++;
+                }
+            }
+            for (int i = 1; i <= n - 1; i++) {
+                cout << i << ' ';
+            }
+            cout << n << endl;
+            cout << num << endl;
+        }
+        //歌曲总数大于4时，显示完全所有歌曲需要翻页，屏幕总是显示4首歌曲
+        else {
+            for (int i = 0; i < order.size(); i++) {
+                if (first == 1 && num == 1 && order[i] == 'U') {
+                    first = n - 3;
+                    num = n;
+                }
+                //特殊翻页1                
+                else if (first == n - 3 && num == n && order[i] == 'D') {
+                    first = 1; num = 1;
+                }
+                //特殊翻页2               
+                else if (first != 1 && num == first && order[i] == 'U') {
+                    first--; num--;
+                }
+                //一般翻页1                
+                else if (first != n - 3 && num == first + 3 && order[i] == 'D') {
+                    first++; num++;
+                }
+                //一般翻页2                
+                else if (order[i] == 'U') {
+                    num--;
+                }
+                //其他情况1                
+                else {
+                    num++;
+                }
+                //其他情况2            
+            }
+            for (int i = first; i < first + 3; i++) {
+                cout << i << ' ';
+            }
+            cout << first + 3 << endl;
+            cout << num << endl;
+        }
+    }
+    return 0;
+}
+
+//第六十二题  MP3光标位置
+#include<iostream>
+#include<string>
+using namespace std;
+void Short(string str, int  n)
+{
+    int initial = 1, current = 1;
+    int len = str.length();
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == 'U')
+        {
+            if (current == 1)
+                current = n;
+            else
+                current--;
+        }
+        else if (str[i] == 'D')
+        {
+            if (current == n)
+                current = 1;
+            else
+                current++;
+        }
+    }
+    for (int i = 1; i < n; i++)
+        cout << i << " ";
+    cout << n << endl;
+    cout << current << endl;
+}
+
+void Long(string str, int n)
+{
+    int initial = 1, current = 1;
+    int len = str.length();
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == 'U')
+        {
+            if (initial == 1 && current == 1)
+            {
+                current = n;
+                initial = n - 3;
+            }
+            else if (initial == current)
+            {
+                current--;
+                initial--;
+            }
+            else
+                current--;
+        }
+        else if (str[i] == 'D')
+        {
+            if (initial == n - 3 && current == n)
+            {
+                current = 1;
+                initial = 1;
+            }
+            else if (initial == current - 3)
+            {
+                current++;
+                initial++;
+            }
+            else
+                current++;
+        }
+    }
+    for (int i = initial; i != initial + 3; i++)
+        cout << i << " ";
+    cout << initial + 3 << endl;
+    cout << current << endl;
+}
+
+int main()
+{
+    int n;
+    while (cin >> n)
+    {
+        string str;
+        cin >> str;
+        if (n <= 4)
+            Short(str, n);
+        else
+            Long(str, n);
+    }
+    return 0;
+}
+
+/********************************************************************************************************************/
+
 
 //单词搜索
 //给定一个二维网格和一个单词，找出该单词是否存在于网格中。
